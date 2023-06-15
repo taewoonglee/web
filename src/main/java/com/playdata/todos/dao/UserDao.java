2fd15327e1a05d5dce880c7f909de3e352fbe855
xodndlee <dnd5436@gmail.com>
2023-06-15 15:05:37 +0900

    15:05

--- src/main/java/com/playdata/todos/dao/UserDao.java
+++ src/main/java/com/playdata/todos/dao/UserDao.java
@@ -3,10 +3,11 @@ package com.playdata.todos.dao;
 import com.playdata.todos.config.JdbcConnection;
 import com.playdata.todos.dto.User;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import static com.playdata.todos.servlet.UserServlet.users;
 import static java.time.chrono.JapaneseEra.values;
 
 public class UserDao {
@@ -30,6 +31,80 @@ public class UserDao {
         }
 
     }
+    public boolean login2(String id, String password)
+    {
+        Connection conn =new JdbcConnection().getJdbc();
+        String sql = "select id, password from users where username = ? and password = ?";
+        try {
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1,id);
+            pst.setString(2,password);
+            ResultSet resultSet = pst.executeQuery();
+            while(resultSet.next())
+            {
+                return true;
+            }
+        }
+        catch (SQLException e){
+            throw new RuntimeException();
+
+        }
+        return false;
 
 
+    }
+    public boolean login(String id, String password)
+    {
+        Connection conn =new JdbcConnection().getJdbc();
+        String sql = "select id, username,name,password,create_at"+
+                "from users" +
+                "where username = ? and password = ?";
+        try {
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1,id);
+            pst.setString(2,password);
+            ResultSet resultSet = pst.executeQuery();
+            while(resultSet.next())
+            {
+                users.add(makeUser(resultSet));
+                return true;
+            }
+
+        }
+        catch (SQLException e){
+            throw new RuntimeException();
+
+        }
+        return users.size()!=0;
+    }
+    private User makeUser(ResultSet resultSet){
+        Integer id;
+        String password, username, name, createAt;
+        try {
+            id = resultSet.getInt("id");
+        }catch (SQLException e) {
+            id = null;
+        }
+        try {
+            password = resultSet.getString("password");
+        }catch (SQLException e) {
+            password = null;
+        }
+        try {
+            username = resultSet.getString("username");
+        }catch (SQLException e) {
+            username = null;
+        }
+        try {
+            name = resultSet.getString("name");
+        }catch (SQLException e) {
+            name = null;
+        }
+        try {
+            createAt = resultSet.getString("createAt");
+        }catch (SQLException e) {
+            createAt = null;
+        }
+        return new User(id,username,password,name,createAt);
+    }
 }
--- src/main/java/com/playdata/todos/servlet/Login.java
+++ src/main/java/com/playdata/todos/servlet/Login.java
@@ -1,36 +1,39 @@
 package com.playdata.todos.servlet;
 
 import com.playdata.todos.dao.UserDao;
-import com.playdata.todos.dto.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-public class UserServlet extends HttpServlet {
-    public static List<User> users = new ArrayList<User>();
+import static com.playdata.todos.servlet.UserServlet.users;
 
+public class Login extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setStatus(200);
-        req.getRequestDispatcher("views/user.html").forward(req,resp);
+        req.getRequestDispatcher("views/login.html").forward(req,resp);
         super.doGet(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         String username = req.getParameter("username");
         String password = req.getParameter("password");
-        String name = req.getParameter("name");
-        User user = new User(null,username,password,name,null);
         UserDao userDao = new UserDao();
-        userDao.insert(user);
-        resp.sendRedirect("/login");  //여기서 다음페이지로 넘어감
-        //super.doPost(req, resp);
-        resp.setStatus(201);
+
+
+        if(userDao.login2(username,password))
+        {
+            System.out.println("로그인");
+            resp.sendRedirect("/main");
+
+        }
+        else {
+            System.out.println("실패");
+            resp.sendRedirect("/user");
+        }
     }
 }
