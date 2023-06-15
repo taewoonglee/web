9d1b16e2453b648ef61b92cfe382db819b50a9c1
xodndlee <dnd5436@gmail.com>
2023-06-15 10:48:18 +0900

    user_post

--- src/main/java/com/playdata/todos/servlet/UserServlet.java
+++ src/main/java/com/playdata/todos/servlet/UserServlet.java
@@ -10,12 +10,21 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(200);
         req.getRequestDispatcher("views/user.html").forward(req,resp);
         super.doGet(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String name = req.getParameter("name");
+        System.out.println(username);
+        System.out.println(password);
+        System.out.println(name);
+        resp.setStatus(201);
+        resp.sendRedirect("/user");  //여기서 다음페이지로 넘어감
+        //super.doPost(req, resp);
     }
 }
