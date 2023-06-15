33138f65c0736430bf2c7be575452330179fbfc3
xodndlee <dnd5436@gmail.com>
2023-06-15 10:05:15 +0900

    111

--- src/main/java/com/playdata/todos/config/JdbcConnection.java
+++ src/main/java/com/playdata/todos/config/JdbcConnection.java
@@ -7,8 +7,7 @@ import java.sql.SQLException;
         private final String url = "jdbc:mysql://127.0.0.1:3306/todos" +
                 "?serverTimezone=Asia/Seoul&characterEncoding=UTF-8";
         private final String root = "root";
-        private final String password = "1q2w3e4r!@";
-
+        private final String password = "1q2w3e
         public Connection getJdbc() {
             Connection conn;
             try {
@@ -17,8 +16,6 @@ import java.sql.SQLException;
             catch (SQLException e) {
                 throw new RuntimeException(e);
             }
-
             return conn;
         }
-
 }
